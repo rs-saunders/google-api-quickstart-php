323e6dfab83a618c3d12c58e8c538290bb00c562
Richard Saunders <richard@sdevsolutions.com>
2017-05-12 21:57:33 +0100

    list messages

--- gmail.messages.php
+++ gmail.messages.php
@@ -7,3 +7,34 @@ $client = getClient();
 $service = new Google_Service_Gmail($client);
 
 $user = 'me';
+
+$messages = listMessages($service, $user);
+
+/**
+ * Get list of Messages in user's mailbox.
+ *
+ * @param  Google_Service_Gmail $service Authorized Gmail API instance.
+ * @param  string $userId User's email address. The special value 'me'
+ * can be used to indicate the authenticated user.
+ * @return array Array of Messages.
+ */
+function listMessages($service, $userId) {
+    $pageToken = NULL;
+    $messages = array();
+    $opt_param = array();
+
+    try {
+        $messagesResponse = $service->users_messages->listUsersMessages($userId, $opt_param);
+        if ($messagesResponse->getMessages()) {
+            $messages = array_merge($messages, $messagesResponse->getMessages());
+        }
+    } catch (Exception $e) {
+        print 'An error occurred: ' . $e->getMessage();
+    }
+
+    foreach ($messages as $message) {
+        print 'Message with ID: ' . $message->getId() . PHP_EOL;
+    }
+
+    return $messages;
+}
